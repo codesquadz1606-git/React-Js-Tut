3839e987d3f5eb2ac3d89d403d92e2422d2ae662
codesquadz1606-git <codesquadz1606@gmail.com>
2026-03-18 10:39:14 +0530

    Fifth

--- Class 5/src/App.jsx
+++ Class 5/src/App.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import "./App.css"
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from "react-icons/io";
+
 
 const App = () => {
-  const PAGE_SIZE=12;
-  let url = "https://dummyjson.com/products?limit=500"
+  const PAGE_SIZE = 12;
   const [products, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  // const [active,setActive]=useState(false)
 
-  const[current,setCurrent]=useState(0);
+  const [current, setCurrent] = useState(0);
+  let url = `https://dummyjson.com/products?limit=${PAGE_SIZE}&skip=${current * PAGE_SIZE}`
   // function dataFetch(){
   //   fetch(url)
   //   .then((res)=>{
@@ -25,41 +30,55 @@ const App = () => {
     // console.log(res.data.products)
     let data = res.data.products
     setProducts(data, ...products)
+    setTotal(res.data.total)
   }
 
   useEffect(() => {
     dataFetch()
-  }, [])
+  }, [current])
 
   console.log(products)
+  console.log(total)
 
-  const noOfPages=Math.ceil(products.length/PAGE_SIZE)
+  const noOfPages = Math.ceil(total / PAGE_SIZE)
   console.log(noOfPages)
 
-  const start=current*PAGE_SIZE
-  const end=start+PAGE_SIZE
+  const start = current * PAGE_SIZE
+  const end = start + PAGE_SIZE
 
   return (
     <div>
-      <div style={{display:"flex",flexWrap:"wrap",gap:"30px",justifyContent:"center"}}>
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "30px", justifyContent: "center" }}>
         {
-          products.slice(start,end).map((el) => (
-            <div style={{width:"300px",padding:"20px",border:"2px solid white",borderRadius:"30px"}}>
+          products.map((el) => (
+            <div style={{ width: "300px", padding: "20px", border: "2px solid white", borderRadius: "30px" }}>
               <h2>{el.title}</h2>
               <img src={el.thumbnail} alt="" />
               <p>{el.description}</p>
-              <p>Rs {Math.ceil(el.price*90)}/-</p>
-              <button style={{padding:"10px 20px"}}>Buy Now</button>
+              <p>Rs {Math.ceil(el.price * 90)}/-</p>
+              <button style={{ padding: "10px 20px" }}>Buy Now</button>
             </div>
           ))
         }
       </div>
-      
-      {
-        Array(noOfPages).keys().map((el)=>(
-          <button onClick={()=>{setCurrent(el)}} style={{marginRight:"20px",padding:"5px 10px",marginTop:"30px"}}>{el+1}</button>
-        ))
-      }
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "30px" }}>
+
+        {/* <button style={{ backgroundColor: "transparent", border: "none" }}> */}
+          <IoIosArrowDropleftCircle disabled={current==0} onClick={() => { setCurrent(current - 1) }} size="30" style={{ marginRight: "20px" }} />
+        {/* </button> */}
+        {
+          Array(noOfPages).keys().map((el) => (
+            <button
+              onClick={() => { setCurrent(el); }}
+              style={{ marginRight: "20px", padding: "5px 10px" }}
+            // className={active?"active":""}
+            >{el + 1}</button>
+          ))
+        }
+        {/* <button disabled={current == noOfPages} style={{ backgroundColor: "transparent", border: "none" }}> */}
+          <IoIosArrowDroprightCircle disabled={current >= noOfPages-1} onClick={() => { setCurrent(current + 1) }} size="30" />
+        {/* </button> */}
+      </div>
     </div>
   )
 }
